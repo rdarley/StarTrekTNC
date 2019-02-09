5045bcac65dadfc093a3130372249b7683a4138e
Ryan Darley <spook6@gmail.com>
2019-02-09 13:16:28 -0600

    Basic contact form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,17 @@
 from flask import render_template
 from app import app
+from app.forms import ContactForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home', welcome_msg='Welcome to Star Trek: The Next Conversation')
+    welcome_msg = 'Welcome to Star Trek: The Next Conversation'
+    return render_template('index.html', title='Home', welcome_msg=welcome_msg)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html',
+                           title='Contact Us',
+                           form=ContactForm())
